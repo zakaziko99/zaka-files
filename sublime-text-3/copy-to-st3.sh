41cebb53cc69f880bcde736f2cfe42f384b896cd
Zakaria Amarifi <z.amarifi@dm73.net>
2016-11-07 22:14:18 +0000

    improve copy snippets script

--- sublime-text-3/copy-to-st3.sh
+++ sublime-text-3/copy-to-st3.sh
@@ -10,4 +10,6 @@ fi
 if [[ -d $TARGET_ST3_SNIPPETS ]]; then
     # \cp is to unalias temporarily the cp -i command
     yes | \cp -R $BASEDIR/snippets-zakaria $TARGET_ST3_SNIPPETS
+else
+    echo "Can't find the sublime text 3 user packages folder"
 fi
