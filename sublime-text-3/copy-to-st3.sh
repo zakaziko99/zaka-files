fd739aaf5bb3a023b877f5589013c72feda6dcb8
Zakaria Amarifi <z.amarifi@dm73.net>
2016-11-07 22:10:59 +0000

    add shell script to copy all the snippets to Sublime Text 3

---
+++ sublime-text-3/copy-to-st3.sh
@@ -0,0 +1,13 @@
+BASEDIR=$(dirname $0)
+
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+    TARGET_ST3_SNIPPETS='~/.config/sublime-text-3/Packages/User'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+    TARGET_ST3_SNIPPETS='~/Library/Application Support/Sublime Text 3/Packages/User'
+fi
+
+if [[ -d $TARGET_ST3_SNIPPETS ]]; then
+    # \cp is to unalias temporarily the cp -i command
+    yes | \cp -R $BASEDIR/snippets-zakaria $TARGET_ST3_SNIPPETS
+fi
