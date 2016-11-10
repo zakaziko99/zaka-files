b3ff338f9ff9bba15a3785c6e72bc94b99c978e0
Zakaria Amarifi <z.amarifi@dm73.net>
2016-11-10 21:48:06 +0000

    improve the copy snippet script from Sublime Text 3

--- sublime-text-3/get-from-st3.sh
+++ sublime-text-3/get-from-st3.sh
@@ -1,21 +1,28 @@
+#!/bin/bash
+
 BASEDIR=$(dirname $0)
+CURRENT_PWD=$(pwd)
+cd ~
+HOME_PWD=$(pwd)
+cd $CURRENT_PWD
 
 unamestr=$(uname)
 if [[ "$unamestr" == 'Linux' ]]; then
-    TARGET_ST3_SNIPPETS='~/.config/sublime-text-3/Packages/User'
+    TARGET_ST3_SNIPPETS="$HOME_PWD/.config/sublime-text-3/Packages/User"
 elif [[ "$unamestr" == 'FreeBSD' ]]; then
     TARGET_ST3_SNIPPETS='~/Library/Application Support/Sublime Text 3/Packages/User'
 fi
 
 if [ -d $TARGET_ST3_SNIPPETS ]; then
     if [[ -n $1 ]]; then
-        if [ -f $TARGET_ST3_SNIPPETS/$1.sublime-snippet ]; then
-            cp $TARGET_ST3_SNIPPETS/$1.sublime-snippet $BASEDIR/snippets-zakaria
+        if [ -f $TARGET_ST3_SNIPPETS/snippets-zakaria/$1 ]; then
+            cp $TARGET_ST3_SNIPPETS/snippets-zakaria/$1 $BASEDIR/snippets-zakaria/$1
         else
-            echo "The file '$TARGET_ST3_SNIPPETS/$1.sublime-snippet' does not exist !"
+            echo "The file '$TARGET_ST3_SNIPPETS/snippets-zakaria/$1' does not exist !"
         fi
     else
-        echo "You must provide the name of the snippet to copy from Sublime Text 3"
+        echo "You must provide the relative path from snippets-zakaria folder"
+        echo "with the name of the snippet to copy from Sublime Text 3"
     fi
 else
     echo "Can't find the Sublime Text 3 user packages folder"
