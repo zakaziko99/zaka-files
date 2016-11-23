91a6320812e6d006243da05182a908daa1f350c1
Zakaria Amarifi <z.amarifi@dm73.net>
2016-11-23 21:02:48 +0000

    fix copy snippets to Sublime Text 3 script

--- sublime-text-3/copy-to-st3.sh
+++ sublime-text-3/copy-to-st3.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 BASEDIR=$(dirname $0)
+CURRENT_PWD=$(pwd)
+cd ~
+HOME_PWD=$(pwd)
+cd $CURRENT_PWD
 
 unamestr=$(uname)
 if [[ "$unamestr" == 'Linux' ]]; then
-    TARGET_ST3_SNIPPETS='~/.config/sublime-text-3/Packages/User'
+    TARGET_ST3_SNIPPETS="$HOME_PWD/.config/sublime-text-3/Packages/User"
 elif [[ "$unamestr" == 'FreeBSD' ]]; then
     TARGET_ST3_SNIPPETS='~/Library/Application Support/Sublime Text 3/Packages/User'
 fi
 
 if [ -d $TARGET_ST3_SNIPPETS ]; then
-    # \cp is to unalias temporarily the cp -i command
     yes | \cp -R $BASEDIR/snippets-zakaria $TARGET_ST3_SNIPPETS
+    # \cp is to unalias temporarily the cp -i command
 else
     echo "Can't find the sublime text 3 user packages folder"
 fi
